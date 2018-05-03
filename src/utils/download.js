27fca0aa5ffc0d20dc110e158d5459ab9c28a044
Morteza Ziaeemehr <m.ziaeemehr@gmail.com>
2018-05-03 22:36:13 +0430

    use axios again. fix close file in download

--- src/utils/download.js
+++ src/utils/download.js
@@ -1,30 +1,42 @@
-const fs = require("fs");
+const Fs = require("fs");
 const Path = require("path");
-const https = require("https");
+const Axios = require("axios");
 
-async function downloadFile({ file_id, telegram }) {
+async function downloadFile({file_id, telegram}) {
   const { file_path } = await telegram.getFile(file_id);
-  const ext = file_path.split(".")[1];
-  const path = Path.resolve(__dirname, "..", "..", "temp", `${file_id}.${ext}`);
-  const url = `https://api.telegram.org/file/bot${
-    process.env.BOT_TOKEN
-  }/${file_path}`;
+  const ext = file_path.split('.')[1];
+  const path = Path.resolve(
+    __dirname,
+    "..",
+    "..",
+    "temp",
+    `${file_id}.${ext}`
+  );
+  const url = `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${file_path}`;
+
+  // axios image download with response type "stream"
+  const response = await Axios({
+    method: "GET",
+    url: url,
+    responseType: "stream"
+  });
+
+  // pipe the result stream into a file on disc
+  const file = Fs.createWriteStream(path);
+  response.data.pipe(file);
 
   // return a promise and resolve when download finishes
   return new Promise((resolve, reject) => {
-    var file = fs.createWriteStream(path);
-    https.get(url, function(response) {
-      response.pipe(file);
-      file.on("finish", function() {
-        file.close(() => {
-          resolve({ local_path: path });
-        });
+    response.data.on("end", () => {
+      file.close(() => {
+        resolve({local_path: path});
       });
+    });
 
-      file.on("error", err => {
-        console.log("error", err);
-        reject();
-      });
+    response.data.on("error", (err) => {
+      console.log('error', err);
+      file.close();
+      reject();
     });
   });
 }
